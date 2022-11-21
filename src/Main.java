800c1e36b2df0a2b607bd971d1a734d52ecaae05
Luciano Yamil Martínez Castro <a22lucianoymc@ateis.local>
2022-11-21 16:57:07 +0100

    añadiendo código para probar las propiedades de Java.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int nota;
 
-        System.out.println("Hola mundo!");
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Ingrese nota de alumno (Número negativo para finalizar)");
+            nota = sc.nextInt();
+            if (nota >= 7 && nota <=10){
+                System.out.println("Bien.");
+            } else if (nota >=4 && nota<7) {
+                System.out.println("Regular.");
+            } else if (nota<4 && nota>0)  {
+                System.out.println("Suspenso.");
+            }
+        } while (nota>0);
 
 
     }
